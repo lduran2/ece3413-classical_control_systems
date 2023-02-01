b45af53f90bdd8cb093bd4a3449298ade0825190
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-02-01 17:49:37 -0500

    [lab01] simple IO model

--- lab01-intro/part02_01_transfer_function_params.m
+++ lab01-intro/part02_01_transfer_function_params.m
@@ -9,7 +9,7 @@
 clear
 
 % H(s) = (ss + 10s + 24)/(s^4 + 26s^3 + 231ss + 766s + 560)
-Hpoly = [0 0 1 10 24 ; 1 26 231 766 560];
+Hpoly = [0 0 1 10 24 ; 1 26 231 766 560]; % [V^0]
 H = tf(Hpoly(1,:), Hpoly(2,:))
 
 % find the poles and zeros
@@ -17,5 +17,6 @@ H_poles = pole(H)
 H_zeros = zero(H)
 
 % parameters of step input
-step_init = 1;
-step_final = 2;
+step_init = 1;  % [V]
+step_final = 2; % [V]
+step_time = 1;  % [s]
