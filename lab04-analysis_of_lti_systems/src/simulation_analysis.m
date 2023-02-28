3d68b79c606433c989182a7ed0ddbc980074f790
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-02-28 18:40:06 -0500

    [lab04] finished simulation analysis

--- lab04-analysis_of_lti_systems/src/simulation_analysis.m
+++ lab04-analysis_of_lti_systems/src/simulation_analysis.m
@@ -1,10 +1,9 @@
 %% simulation_analysis.m
-% Sets the paraeters for a second order transfer function, then uses step
+% Sets the parameters for a second order transfer function, then uses step
 % analysis to characterize the system.
 % By      : Leomar Duran
-% When    : 2023-03-27t23:23
+% When    : 2023-03-28t18:39
 % For     : ECE 3413 Classical Control Systems
-% Version : 0.0.1
 %
 
 clear
@@ -18,6 +17,7 @@ TSTOP = 10.0    % [s]
 
 % step function parameters
 Tstep = 0       % [s]
+stepFinal = 1   % [V]
 
 % transfer function parameters
 B = 2;
@@ -42,14 +42,48 @@ t = data.ans.Time;
 
 
 %% part01.3 perform step analysis
-[peak, peakIdx] = max(c)
+[peak, peakIdx] = max(c);
 % percent overshoot = (peak value - final value)/(final value) * 100%
-pcOS = (peak - c(end))/c(end)* 100
+pcOS = (peak - c(end))/c(end)* 100;
 % rise time = time for output to go from 10% to 90% of the final value
 pc10Idx = find(c >= .10*c(end), 1)
 pc90Idx = find(c >= .90*c(end), 1)
-Tr = t(pc90Idx) - t(pc10Idx)
-% peak time
-Tp = t(peakIdx)
-% settling time
+Tr = t(pc90Idx) - t(pc10Idx);
+% peak time = time for the output to reach its maximum value
+Tp = t(peakIdx);
+% settling time = time for output to be bound within 5% of its final value
+TsIdx = find(abs(c - c(end)) >= 0.05*c(end), 1, 'last')
+Ts = t(TsIdx);
 % steady state error
+Ess = (stepFinal - c(end));
+stepCharacteristics = struct('peak', peak, 'pcOS', pcOS, 'Tr', Tr, 'Tp', Tp, 'Ts', Ts, 'Ess', Ess)
+
+% plot the steady state error
+subplot(2,1,1)
+hold on
+plot(data.ans, 'LineWidth', 2)
+plot(t([1 end]), [1 1]*stepFinal)
+plot([1 1]*t(end), c(end) + [0 Ess])
+hold off
+title('comparison of input and response')
+xlabel('time [s]')
+ylabel('voltage [V]')
+legend('step response', 'input signal', ...
+    'steady state error')
+%
+
+% plot with these characteristics labeled
+subplot(2,1,2)
+plot(data.ans, 'LineWidth', 2)
+hold on
+plot([1 1 0]*Tp, [0 1 1]*peak)
+plot([1 1 0]*t(pc10Idx), [0 1 1]*c(pc10Idx))
+plot([1 1 0]*t(pc90Idx), [0 1 1]*c(pc90Idx))
+hold off
+title('response to input with characteristics')
+xlabel('time [s]')
+ylabel('voltage [V]')
+legend('step response', 'peak', ...
+    'settling lower limit', 'settling upper limit')
+%
+
