e3e2b3776982b6f3552f66fb1418d8b771e0f39a
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-02-28 19:11:12 -0500

    [lab04] sinusoid saver implemented

---
+++ lab04-analysis_of_lti_systems/src/part02_save_sinusoid.m
@@ -0,0 +1,17 @@
+%% part02_save_sinusoid.m
+% Creates and saves a sinusoidal function for simulation
+% By      : Leomar Duran
+% When    : 2023-03-28t19:07
+% For     : ECE 3413 Classical Control Systems
+%
+
+clear
+
+t = linspace(0,10,1000);
+y = 0.5*cos(t) - 0.3;
+y_ts = timeseries(y, t)
+
+save input.mat -v7.3 y_ts
+
+% clear again to make sure y_t is not in memory
+clear
