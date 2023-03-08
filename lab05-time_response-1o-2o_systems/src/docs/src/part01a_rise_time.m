4c92b9c14a5d1df9603c93a316d76ef07bdf880c
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-03-08 18:36:51 -0500

    [lab05] doc'd fixed deconvbyrow by padding

--- lab05-time_response-1o-2o_systems/src/docs/src/part01a_rise_time.m
+++ lab05-time_response-1o-2o_systems/src/docs/src/part01a_rise_time.m
@@ -2,7 +2,7 @@
 % Calculates the rise time Tr given the following transfer function
 % denominator linear coefficient, a, and sinusoidal frequency, w.
 % By      : Leomar Duran
-% When    : 2023-03-08t15:18
+% When    : 2023-03-08t18:29
 % For     : ECE 3413 Classical Control Systems
 %
 
@@ -61,15 +61,16 @@ function [acc,rem] = convpow(init, base, power)
 
     % if power is negative
     if (power < 0)
+        % initialize a one vector with the same # rows as base
+        onesInit = ones(size(base, 1), 1);
         % find the corresponding positive convolution power and
         % deconvolve against the initial value
-        [acc, rem] = deconv(init, convpow(1, base, -power));
+        [acc, rem] = deconvbyrow(init, convpow(onesInit, base, -power));
         return
     end % if (power < 0)
 
     % initialize
     acc = init;
-    rem = 0;
     % half power until 0
     while (power > 0)
         % if odd power
@@ -85,14 +86,17 @@ function [acc,rem] = convpow(init, base, power)
         % update power
         power = bitshift(power, -1);
     end % while (n > 0)
+    % remainder is all zeroes bcs the power is non-negative
+    rem = zeros(size(acc));
 end % function conv_rows(matrix)
 
 function UV = convbyrow(U, V)
 %% convbyrow(U, V)
-% Performs rowwise convolution of the given matrices U, V
+% Performs rowwise convolution of the given matrices U, V.
 %
 %% Input Arguments
 % *U* : first input matrix
+%
 % *V* : second input matrix
 %
 %% Output Arguments
@@ -102,7 +106,9 @@ function UV = convbyrow(U, V)
     % get the sizes of the arrays
     [nURows, nUCols] = size(U);
     [nVRows, nVCols] = size(V);
-    assert(nURows == nVRows, 'U, V must have an equal number of rows.')
+    assert(nURows == nVRows, ...
+        sprintf('U = %d, V = %d must have an equal number of rows.', ...
+            nURows, nVRows))
 
     % separate the rows
     URows = mat2cell(U, ones(nURows, 1), nUCols);
@@ -114,4 +120,80 @@ function UV = convbyrow(U, V)
     UVRows = cellfun(xConv, URows, VRows, 'UniformOutput', false);
     % convert to matrix
     UV = cell2mat(UVRows);
-end % function conv_rows(matrix)
+end % function convbyrow(U, V)
+
+function [Q,R] = deconvbyrow(U, V)
+%% deconvbyrow(U, V)
+% Performs rowwise deconvolution of the given matrices U, V.
+%
+%% Input Arguments
+% *U* : first input matrix
+%
+% *V* : second input matrix
+%
+%% Output Arguments
+% *Q* : the matrix of quotient rows
+%
+% *R* : the matrix of remainder rows
+%
+
+    % get the sizes of the arrays
+    [nURows, nUCols] = size(U);
+    [nVRows, nVCols] = size(V);
+    assert(nURows == nVRows, ...
+        sprintf('U = %d, V = %d must have an equal number of rows.', ...
+            nURows, nVRows))
+
+    % separate the rows
+    URows = mat2cell(U, ones(nURows, 1), nUCols);
+    VRows = mat2cell(V, ones(nURows, 1), nVCols);
+
+    % deconvolution function handle
+    xDeconv = @(u, v) deconvlead(u, v);
+    % apply to rows
+    [QRows, RRows] = cellfun(xDeconv, URows, VRows, 'UniformOutput', false)
+    % convert to matrices
+    Q = cell2mat(QRows);
+    R = cell2mat(RRows);
+end % function deconvbyrow(U, V)
+
+function [q, r] = deconvlead(u, v)
+%% deconvlead(u, v)
+% Performs deconvolution accounting for leading zeros.  Leading zeros
+% are ignored in inputs, and the output is padded to match the inputs.
+% E.g., each of the following will produce results of size 3
+%       [1, 0, 0]/[1, 0]
+%       [1, 0, 0]/[0, 1]
+%       [0, 1, 0]/[1, 0]
+%       [0, 1, 0]/[0, 1]
+%       [0, 0, 1]/[1, 0]
+%       [0, 0, 1]/[0, 1]
+%
+%% Input Arguments
+% *u* : first input row vector
+%
+% *v* : second input row vector
+%
+%% Output Arguments
+% *q* : the quotient row vector
+%
+% *r* : the remainder row vector
+%
+
+    % get the first nonzero of each vector
+    uIdx = find(u, 1);
+    vIdx = find(v, 1);
+
+    % use the dividend as the ideal size for quotient and remainder
+    % vectors
+    uLen = numel(u)
+
+    % perform the deconvolution
+    [q, r] = deconv(u(uIdx:end), v(vIdx:end))
+    % get the lengths of the output vectors
+    qLen = numel(q);
+    rLen = numel(r);
+    % zero pad each
+    q = [zeros(1, (uLen - qLen)) q]
+    r = [zeros(1, (uLen - rLen)) r]
+end % function function deconvlead(U, V)
