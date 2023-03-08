2910bc9988c12a9758c4019e7893179b7bdfa0a9
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-03-08 13:49:41 -0500

    [lab05] modeling G2 in part01a

--- lab05-time_response-1o-2o_systems/src/part01a_rise_time.m
+++ lab05-time_response-1o-2o_systems/src/part01a_rise_time.m
@@ -13,6 +13,7 @@ clear
 % s.t.
 a = 4           % linear coefficient of transfer function denominator
 w = sqrt(21)    % sinusoidal frequency
+G2 = tf((a/2)^2 + w^2, (convpow(1, [1 a/2], 2) + convpow(1, [0 w], 2)) )
 
 % set up time domain
 syms t          % symbol for time [s]
@@ -32,3 +33,47 @@ TrLims = arrayfun(xT_c, cf*[.1 .9])
 
 % find rise time Tr = -t(.1)c + t(.9)c
 Tr = TrLims*[-1 1]'
+
+%% end
+
+function acc = convpow(init, base, power)
+%% convpow(vector, power)
+% Finds the convolution power $\mathbb{a}\mathbb{b}^{*n}$, that is,
+% repeatedly convolves the given base row vector as many times as the
+% given power.
+%
+%% Input Arguments
+% *init* : double = initial accumulation value, $\mathbb{a}$
+% *base* : double = row vector to convolve $\mathbb{b}$
+% *power* : int = the number of times to convolve $n$
+%
+%% Output Arguments
+% *acc* : the resulting convolved row vector
+%
+
+    % if power is negative
+    if (power < 0)
+        % find the corresponding positive convolution power and
+        % deconvolve against the initial value
+        acc = deconv(init, convpow(1, base, -power));
+        return
+    end % if (power < 0)
+
+    % initialize
+    acc = init;
+    % half power until 0
+    while (power > 0)
+        % if odd power
+        if (bitand(power, 1))
+            % accumulate
+            acc = conv(acc, base);
+            % even out power
+            power = bitxor(power, 1);
+        end % if (bitand(power, 1))
+
+        % convolve square the vector
+        base = conv(base, base);
+        % update power
+        power = bitshift(power, -1);
+    end % while (n > 0)
+end % function conv_rows(matrix)
