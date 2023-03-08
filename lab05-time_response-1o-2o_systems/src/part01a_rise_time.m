c6bbed41502dcd2975e1cd6682f8ff77724a3a0b
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-03-08 15:20:36 -0500

    [lab05] convpow can convolve by rows now

--- lab05-time_response-1o-2o_systems/src/part01a_rise_time.m
+++ lab05-time_response-1o-2o_systems/src/part01a_rise_time.m
@@ -2,7 +2,7 @@
 % Calculates the rise time Tr given the following transfer function
 % denominator linear coefficient, a, and sinusoidal frequency, w.
 % By      : Leomar Duran
-% When    : 2023-03-07t23:21
+% When    : 2023-03-08t15:18
 % For     : ECE 3413 Classical Control Systems
 %
 
@@ -13,7 +13,7 @@ clear
 % s.t.
 a = 4           % linear coefficient of transfer function denominator
 w = sqrt(21)    % sinusoidal frequency
-G2 = tf((a/2)^2 + w^2, (convpow(1, [1 a/2], 2) + convpow(1, [0 w], 2)) )
+G2 = tf((a/2)^2 + w^2, sum(convpow([1 1]', [1 a/2; 0 w], 2)) )
 
 % set up time domain
 syms t          % symbol for time [s]
@@ -36,44 +36,82 @@ Tr = TrLims*[-1 1]'
 
 %% end
 
-function acc = convpow(init, base, power)
+function [acc,rem] = convpow(init, base, power)
 %% convpow(vector, power)
 % Finds the convolution power $\mathbb{a}\mathbb{b}^{*n}$, that is,
 % repeatedly convolves the given base row vector as many times as the
-% given power.
+% given power to find 
 %
 %% Input Arguments
-% *init* : double = initial accumulation value, $\mathbb{a}$
+% *init* : double = initial accumulation value $\mathbb{a}$
+%
 % *base* : double = row vector to convolve $\mathbb{b}$
+%
 % *power* : int = the number of times to convolve $n$
 %
 %% Output Arguments
 % *acc* : the resulting convolved row vector
+%
+% $\mathbb{p} := \mathbb{a}\mathbb{b}^{*n}$
+%
+% *rem* : the remainder of convolution
+%
+% $\left[\mathbf{0}^\top \middle| \mathbb{a}\right] - \mathbb{p}\mathbb{b}^{*-n}$
 %
 
     % if power is negative
     if (power < 0)
         % find the corresponding positive convolution power and
         % deconvolve against the initial value
-        acc = deconv(init, convpow(1, base, -power));
+        [acc, rem] = deconv(init, convpow(1, base, -power));
         return
     end % if (power < 0)
 
     % initialize
     acc = init;
+    rem = 0;
     % half power until 0
     while (power > 0)
         % if odd power
         if (bitand(power, 1))
             % accumulate
-            acc = conv(acc, base);
+            acc = convbyrow(acc, base);
             % even out power
             power = bitxor(power, 1);
         end % if (bitand(power, 1))
 
         % convolve square the vector
-        base = conv(base, base);
+        base = convbyrow(base, base);
         % update power
         power = bitshift(power, -1);
     end % while (n > 0)
+end % function conv_rows(matrix)
+
+function UV = convbyrow(U, V)
+%% convbyrow(U, V)
+% Performs rowwise convolution of the given matrices U, V
+%
+%% Input Arguments
+% *U* : first input matrix
+% *V* : second input matrix
+%
+%% Output Arguments
+% *UV* : the resulting matrix of convolved rows
+%
+
+    % get the sizes of the arrays
+    [nURows, nUCols] = size(U);
+    [nVRows, nVCols] = size(V);
+    assert(nURows == nVRows, 'U, V must have an equal number of rows.')
+
+    % separate the rows
+    URows = mat2cell(U, ones(nURows, 1), nUCols);
+    VRows = mat2cell(V, ones(nURows, 1), nVCols);
+
+    % convolution function handle
+    xConv = @(U, V) conv(U, V);
+    % apply to rows
+    UVRows = cellfun(xConv, URows, VRows, 'UniformOutput', false);
+    % convert to matrix
+    UV = cell2mat(UVRows);
 end % function conv_rows(matrix)
