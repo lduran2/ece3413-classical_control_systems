581eb32df9bd240089618da75e9fa0dd072f7b90
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-03-29 02:08:55 -0400

    [lab06] clone task 02-01 critically damped -> task 02-02 Routh=Hurwitz

---
+++ lab06-loop_gain_stability-negative_feedback/src/task02_02_routh_hurwitz_mlx.m
@@ -0,0 +1,49 @@
+%% Finding the critical damping points of the given negative feedback system.
+
+% task02_01_critical_damping_points_mlx.m
+% By      : Leomar Duran
+% When    : 2023-03-29t01:39
+% For     : ECE 3413 Classical Control Systems
+%
+
+clear
+
+%% Symbolic representation of the system
+% Given the negative feedback system with
+
+task02_00_simulation_params
+
+%%
+% The equivalent transfer function
+
+C_per_R_s = simplify(G_s/(1 + G_s*H_s))
+
+%% The limit of K
+% We start by solving for $K$ in terms of $s$.
+
+% The transfer function is some polynomial function Den(s) s.t.
+% (C/R)(s) = K/Den(s).
+% Thus Den(s) = K/(C/R)(s)
+
+Den_s = K/C_per_R_s
+
+%%
+% We find the K for characteristic equation $Den(s) = 0$.
+
+K_0 = solve(Den_s == 0, K)
+
+%%
+% We next fix the derivative $\frac{dK_0}{ds} = 0$. So the points of
+% inflection are at
+
+s_0 = roots(sym2poly(diff(K_0, s)))
+
+%%
+% Finally, we find the non-zero values of K at these points of inflection,
+% giving the critical damping point(s).
+
+K_inflection = subs(K_0, s, s_0);
+K_critical_damping = K_inflection(find(K_inflection))
+
+%%
+% Lower values will be overdamped.
