a0ed02991fe5bb366b3b8c643a22d66eb858c625
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-04-05 21:34:58 -0400

    [lab07] error analysis: up to G_CL

---
+++ lab04-analysis_of_lti_systems/src/lab07_task01_error_analysis.m
@@ -0,0 +1,27 @@
+%% lab07_task01_error_analysis.m
+% Performs error analysis on the negative feedback systems given by the
+% transfer functions.
+% By      : Leomar Duran
+% When    : 2023-04-05t21:34
+% For     : ECE 3413 Classical Control Systems
+%
+
+clear
+
+% gains
+syms k
+% input
+syms s
+
+% systems
+% forward branch
+G_s = (k*(s + 6)/poly2sym(poly(-[4 7 9 12]), s)) * ...
+    [
+        1, ...
+        (s + 8) * [1, (s + 1)]
+    ]'
+% backward branch
+H_s = ones(size(G_s))
+
+% equivalent transfer function
+G_CL_s = ((G_s.*H_s).^-1 + 1).^-1
