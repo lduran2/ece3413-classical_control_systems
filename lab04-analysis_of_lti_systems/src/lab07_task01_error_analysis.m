a4e8f93607f2bad17587b99a1dc5425826890aac
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-04-05 22:21:12 -0400

    [lab07] convert error analysis into livescript

--- lab04-analysis_of_lti_systems/src/lab07_task01_error_analysis.m
+++ lab04-analysis_of_lti_systems/src/lab07_task01_error_analysis.m
@@ -1,4 +1,6 @@
-%% lab07_task01_error_analysis.m
+%% Task 01 Error Analysis
+
+% lab07_task01_error_analysis_mlx.m
 % Performs error analysis on the negative feedback systems given by the
 % transfer functions.
 % By      : Leomar Duran
@@ -8,6 +10,8 @@
 
 clear
 
+%% Setup
+
 % gains
 syms k real
 % frequency domain
@@ -15,22 +19,46 @@ syms s
 % time domain
 syms t real
 
-% systems
+%% Systems
+% We have the systems with each of the forward branches
 % forward branch
 G_s = (k*(s + 6)/poly2sym(poly(-[4 7 9 12]), s)) * ...
     [
-        1, ...
-        (s + 8) * [1, (s + 1)]
-    ]'
-% backward branch
+        1; ...
+        (s + 8) * [1; (s + 1)]
+    ]
+%%
+% and each of the backward branches
 H_s = ones(size(G_s))
 
-% equivalent transfer function
+%%
+% being equivalent to the transfer functions
 G_CL_s = ((G_s.*H_s).^-1 + 1).^-1
+
+%% Static error constants
+% Next we calculate the static error constants for position, velocity and
+% acceleration, respectively 
+%
+% $$ K_p = \lim_{s\to0} G(s) $$
+%
+% $$ K_v = \lim_{s\to0} s G(s) $$
+%
+% $$ K_a = \lim_{s\to0} s^2 G(s) $$
 
 % calculate the static error constants
 derivativeNo = (0:2)'
 staticErrorConstants = limit(s.^(derivativeNo)*G_s', s, 0)
 
-% calculate the steady-state errors
+%% Steady-state errors
+% Next we calculate the steady-state errors. These are
+%
+% For step:
+% $e_{ss} = \frac1{1 + K_p}$
+%
+% For ramp:
+% $e_{ss} = \frac1{K_v}$
+%
+% For parabola:
+% $e_{ss} = \frac1{K_a}$
+
 ess = 1./([1 0 0]' + staticErrorConstants)
