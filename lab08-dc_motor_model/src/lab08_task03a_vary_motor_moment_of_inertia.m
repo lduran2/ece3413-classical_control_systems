914bb48d2b9e1af34cf659ec711278400cbd2711
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-04-12 22:00:39 -0400

    [lab08] plot angular velocity, vary moments of inertia

--- lab08-dc_motor_model/src/lab08_task03a_vary_motor_moment_of_inertia.m
+++ lab08-dc_motor_model/src/lab08_task03a_vary_motor_moment_of_inertia.m
@@ -10,20 +10,17 @@
 clear
 lab08_task00_default_dc_motor_motor_params;
 
-K_values = [0.01 0.02 0.3 0.4 1 2]
-% for each K value
-for K=K_values
-    % setting the armature torque constant, back EMF accordingly
-    Kt = K
-    Ke = K
+moments_of_inertia = [0.1 1 5]
+% for each moment of inertia
+for J=moments_of_inertia
     % run the simulation
     simout = sim('lab08_openloop_control_slx', StopTime)
     % plot the resulting time series
     hold on
     plot(simout.simout)
     hold off
-end % next K
-legend(arrayfun((@(x) sprintf("K=%.4e", x)), K_values))
-title('angular velocity response, varying the gains')
+end % next J
+legend(arrayfun((@(x) sprintf("J=%.4e", x)), moments_of_inertia))
+title('angular velocity response, varying the moment of inertia')
 xlabel('time [s]')
 ylabel('angular velocity response [rad/s]')
