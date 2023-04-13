d5355c0a840a887e42c60167cf164816e49d39be
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-04-12 22:15:44 -0400

    [lab08] plotting angular position

---
+++ lab08-dc_motor_model/src/lab08_task05_angle_repeat_params.m
@@ -0,0 +1,15 @@
+%% lab08_task05_angle_repeat_params.m
+% Sets the parameters for the DC motor model so that angle shows to be in
+% [0, 2pi[
+% By      : Leomar Duran
+% When    : 2023-04-12t22:14Q
+% For     : ECE 3413 Classical Control Systems
+%
+
+% set defaults
+clear
+lab08_task00_default_dc_motor_motor_params
+
+% simulation runtime [s]
+% to show the repetition
+StopTime = 100.0
