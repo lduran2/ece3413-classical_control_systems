41aeb0cbc6cec36d6a4ad4348f803ef521b525b0
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-04-12 22:37:34 -0400

    [lab08] started report, cloned from lab 06

---
+++ lab08-dc_motor_model/src/docs/lab08/src/task02_00_simulation_params.m
@@ -0,0 +1,21 @@
+%% task02_00_simulation_params.m
+% Sets the parameters for symbolically modelling a negative feedback
+% system.
+% By      : Leomar Duran
+% When    : 2023-03-29t01:38
+% For     : ECE 3413 Classical Control Systems
+%
+
+clear
+
+% basic symbols:
+% complex frequency
+syms s
+% gain in R
+syms K real
+
+%plant
+G_s = K/(s*(s + 2)^2)
+
+%controller
+H_s = 1
