4a6a619f973ac81ab1af1397449965f3defcead0
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-04-21 19:47:56 -0400

    [lab09] added shouldPidBeOn switch, updated lab08 task 00 params

--- lab08-dc_motor_model/src/lab08_task00_default_dc_motor_motor_params.m
+++ lab08-dc_motor_model/src/lab08_task00_default_dc_motor_motor_params.m
@@ -1,13 +1,23 @@
 %% lab08_task00_default_dc_motor_motor_params.m
 % Sets the default parameters for the DC motor model.
 % By      : Leomar Duran
-% When    : 2023-04-12t10:42Q
+% Version : v2.0.0
 % For     : ECE 3413 Classical Control Systems
 %
+% CHANGELOG:
+%       v2.0.0 - 2023-04-21t19:12Q
+%           added support for PID controllers, sampling frequency for
+%           lab 09
+% 
+%       v1.0.0 - 2023-04-12t10:42Q
+%           complete parameters for lab 08
+% 
 
 clear
 
-% simulation runtime [s]
+% simulation runtime (start) [s]
+StartTime = 0.0
+% simulation runtime (finish) [s]
 StopTime = 10.0
 
 % moment of inertia of rotor [kg m^2]
@@ -22,3 +32,13 @@ Ke = Kt
 R = 1
 % inductance [H]
 L = 0.5
+
+% disable PID
+shouldPidBeOn = false
+% reset all other PID parameters (don't care since PID is disabled)
+P = 0;
+I = 0;
+D = 0;
+
+% sampling frequency [Hz]
+Fs = 0.001
