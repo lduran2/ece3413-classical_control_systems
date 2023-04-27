20edd9b5bff15337177e880fc8de6e087c31d1a3
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-04-26 22:34:16 -0400

    [lab10] smoothened data

--- lab10-closed_loop_gain_determination-root_locus/src/week01_task01.m
+++ lab10-closed_loop_gain_determination-root_locus/src/week01_task01.m
@@ -1,4 +1,4 @@
-%% week01_task01.m
+%% week01_task01_params.m
 % Analyzing the input data.
 % By      : Leomar Duran
 % When    : 2023-04-19t21:40Q
@@ -20,24 +20,32 @@ StartTime = t(1)
 Tsamp = (t(end) - StartTime)/Nsamp
 
 % get the output data
-output = output_load.raw.Data;
+output_raw = output_load.raw.Data;
 % recreate the step input
-input_step = ones(size(output));
+input_step = ones(size(output_raw));
 % as a time series
 input_step_ts = timeseries(input_step, t)
+
+% apply the median filter
+% Find a way to filter the data to the best of your abilities and
+% retain the rise time and settling time characteristics.
+% However, to find the rise and settling time characteristics, we have
+% to smoothen the curve in the first place.  In light of this, a median
+% filter with 19o seems to smoothen out the function satisfactorily by
+% inspection.
+output_response = medfilt1(output_raw, 19);
 
 % plot the data for inspection
 hold on
 plot(input_step_ts)
+pOutput = plot(t, output_response);
+pOutput.LineWidth = 8;
 plot(output_load.raw)
 hold off
 title('signal vs time')
 xlabel('time [s]')
 ylabel('signal <1>')
-legend(["step input", "output data"])
+legend(["step input", "filtered output", "unfiltered output data"])
 
 % create workspace structure
-data = table(input_step, output);
-
-% open System Identification Toolkit
-% ident
+data = table(input_step, output_response);
